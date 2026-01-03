070d2e9b18dab52f04ca352a4f9a232ab000d9ea
utkarshyadav11 <utkarshyadav6745@gmail.com>
2026-01-03 11:29:01 +0530

    Fix SameAsConfig type by making optional social links

--- src/types/config.types.ts
+++ src/types/config.types.ts
@@ -142,9 +142,9 @@ export type SchemaConfig = {
  * Social links for organization.
  */
 export type SameAsConfig = {
-  threads: string;
+  threads?: string;
   linkedin: string;
-  discord: string;
+  discord?: string;
 };
 
 /**
